f55ed0c174e4df07e3d72be9d63796b2a254507f
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 22:44:29 +1300

    create while loop which iterates through and prints the items in an array

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,18 @@ fn main() {
     }
     println!("End count = {count}");
     */
+    /* While loop 
     let mut number = 3;
     while number != 0 {
         println!("{number}");
         number -= 1;
     }
     println!("LIFTOFF!!!");
+    */
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+        index += 1;
+    }
 }
