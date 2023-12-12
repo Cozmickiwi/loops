e5230b660f94c3742bfb6cb1b9ac0998b13ca154
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 22:18:58 +1300

    create simple loop with counter variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        println!("{counter}");
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {result}");
 }
